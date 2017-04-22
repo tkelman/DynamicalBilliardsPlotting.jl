aa7b61f57c5ff793d0c16fbcf268293cfe87cc7f
George Datseris <datseris.george@gmail.com>
2017-04-22 10:37:08 +0200

    added basic source and test code

---
+++ src/DynamicalBilliardsPlotting.jl
@@ -0,0 +1,8 @@
+"""
+Plotting routines and dependencies for the julia package DynamicalBilliards.jl
+"""
+module DynamicalBilliardsPlotting
+
+include("PlotBilliards.jl")
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,3 @@
+using DynamicalBilliards
+using DynamicalBilliardsPlotting
+using Base.Test
